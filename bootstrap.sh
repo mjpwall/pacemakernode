b43a86bf6cbf1c3365e0dc3a8947d4cc1e37fe8a
michaelwall <mjpwall@gmail.com>
2015-01-12 09:08:50 +0000

    added git to vagrant image

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #sudo apt-get install -y python-software-properties
 apt-get update
+apt-get install -y git
 apt-get install -y gcc make build-essential
 add-apt-repository ppa:chris-lea/node.js 
 apt-get update
